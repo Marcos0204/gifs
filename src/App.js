0cdcd13cd39bb69a03bbe4f41e5e769f510304db
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-08 09:54:59 -0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+const GIFS = ['https://media4.giphy.com/media/YPzzY7lbFBB1fxXaQT/giphy.gif?cid=790b761162fa8a5f68428f6daf299f5e16012a638b9ae550&rid=giphy.gif&ct=g']
+
 const  App = () => {
+  const [ gifs, setGifs ] = useState([GIFS])
+  
   return (
     <div className="App">
       <section className="App-content">
-        gifs
+        {
+          [1, 2].map(sigleGifs => <img src={gifs} /> )
+        }
       </section>
     </div>
   );
