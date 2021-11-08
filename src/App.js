9cf67aa8a0f4c865ccd51da1be36aabdc7e3ed6b
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-08 10:49:30 -0400

    fetch-ap_url-data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const GIFS = ['https://media4.giphy.com/media/YPzzY7lbFBB1fxXaQT/giphy.gif?cid=790b761162fa8a5f68428f6daf299f5e16012a638b9ae550&rid=giphy.gif&ct=g']
+const apiURL = 'https://api.giphy.com/v1/gifs/search?api_key=gu8hS8GASdStQPdF1rMsxSXyy8SwlZ27&q=pandas&limit=25&offset=0&rating=g&lang=en'
+
 
 const  App = () => {
-  const [ gifs, setGifs ] = useState([GIFS])
+  const [ gifs, setGifs ] = useState([])
+
+  useEffect(() =>{
+    fetch(apiURL)
+    .then(res => res.json())
+    .then(response =>{
+      const { data } = response;
+      if( Array.isArray(data)) {
+        const gifs = data.map(image => image.images.downsized_medium.url)
+        setGifs(gifs)
+      }
+    })
+  }, [])
   
   return (
     <div className="App">
       <section className="App-content">
         {
-          [1, 2].map(sigleGifs => <img src={gifs} /> )
+          gifs.map((sigleGifs, index) => <img src={sigleGifs} key={index} alt='img panda'/> )
         }
       </section>
     </div>
