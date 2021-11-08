5635edd4a6062162fd318449b3a6da3e2dc67cf3
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-08 17:01:50 -0400

    update-structure

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -3,18 +3,23 @@ import getGifs from '../services/getGifs'
 import Gif from './Gif';
 
 const ListOfGifs = ({params}) => {
-    const [ gifs, setGifs ] = useState([])
+    const [ gifs, setGifs ] = useState([]);
+    const [loading, setLoading] = useState(false)
     const {keyWord} = params
   useEffect( () =>{
+    setLoading(true)
     async function fetchAPi() {
       //const keyWord='morty'
       const data = await getGifs({keyWord});
       setGifs(data)
+      setLoading(false)
     }
     fetchAPi()
 
           //  getGifs.then(res => setGifs(res))    
   }, [keyWord])
+
+    if(loading) return <i>Cargando</i>
 
     return <>
         {gifs.map((singleGifs, index) => <Gif {...singleGifs} key={index}/>  )}
